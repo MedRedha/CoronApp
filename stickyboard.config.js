2fe03d7f1c35e87a49abedb6ad74d6918239c094
Med Redha <redha.dirac@gmail.com>
2020-03-20 15:50:03 +0100

    adding project description module export :speech_balloon:

--- stickyboard.config.js
+++ stickyboard.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     title: 'CoronApp',
-    description: 'Global Corona Dashboard powered by StickyBoard',
+    description:
+        'Global dashboard for monitoring Corona virus (COVID-19) tweaked by The WuuD® Team',
     meta_image: '/static/image/CoronaBoard_preview.png',
     favicon: '/static/image/favicon.png',
 };
