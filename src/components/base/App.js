0b2c3bb8028f94ea24f281f5ad5cfbcf5b78e471
Med Redha <redha.dirac@gmail.com>
2020-03-20 19:56:23 +0100

    a lot of fixes :ambulance: :tada:

--- src/components/base/App.js
+++ src/components/base/App.js
@@ -3,7 +3,12 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Redirect,
+} from 'react-router-dom';
 import loadable from '@loadable/component';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
@@ -12,7 +17,9 @@ import Layout from './Layout';
 import FullScreenLayout from './FullScreenLayout';
 // Statistics
 const DashboardPage = loadable(() => import('components/page/DashboardPage'));
-const RiskRankingPage = loadable(() => import('components/page/RiskRankingPage'))
+const RiskRankingPage = loadable(() =>
+    import('components/page/RiskRankingPage')
+);
 // Not found
 const NotFoundPage = loadable(() => import('components/page/NotFoundPage'));
 
@@ -31,66 +38,79 @@ class App extends React.Component {
 
         const initialThemeKey = LocalStorageManager.getItem(
             LocalStorageConst.KEY.THEME_KEY,
-            themeKeys[0]);
-        const initialTheme = StickyBoardThemes[initialThemeKey] || StickyBoardThemes[themeKeys[0]];
+            themeKeys[0]
+        );
+        const initialTheme =
+            StickyBoardThemes[initialThemeKey] ||
+            StickyBoardThemes[themeKeys[0]];
 
         this.state = {
             selectedThemeKey: initialThemeKey,
             muiTheme: createMuiTheme(initialTheme),
-        }
+        };
     }
 
     onThemeChange = (themeKey) => {
         const selectedTheme = StickyBoardThemes[themeKey];
 
-        this.setState({
-            selectedThemeKey: themeKey,
-            muiTheme: createMuiTheme(selectedTheme)
-        }, () => {
-            LocalStorageManager.setItem(
-                LocalStorageConst.KEY.THEME_KEY,
-                themeKey);
-        });
-    }
+        this.setState(
+            {
+                selectedThemeKey: themeKey,
+                muiTheme: createMuiTheme(selectedTheme),
+            },
+            () => {
+                LocalStorageManager.setItem(
+                    LocalStorageConst.KEY.THEME_KEY,
+                    themeKey
+                );
+            }
+        );
+    };
 
     render() {
-        const {
-            selectedThemeKey,
-            muiTheme,
-        } = this.state;
+        const { selectedThemeKey, muiTheme } = this.state;
 
         return (
             <MuiThemeProvider theme={muiTheme}>
                 <Router>
                     <Switch>
                         {/* Signing pages (FullScreenLayout) */}
-                        {false && <Route path={['/signin', '/signup']}>
-                            <FullScreenLayout>
-                                <Switch>
-                                </Switch>
-                            </FullScreenLayout>
-                        </Route>}
+                        {false}
 
                         {/* other pages (Layout) */}
-                        <Route path='/'>
+                        <Route path="/">
                             <Layout
                                 themeKeys={themeKeys}
                                 selectedThemeKey={selectedThemeKey}
                                 onThemeChange={this.onThemeChange}>
                                 <Switch>
-                                    <Route exact path='/' component={() => { return <Redirect to='/statistics/dashboard' /> }} />
+                                    <Route
+                                        exact
+                                        path="/"
+                                        component={() => {
+                                            return (
+                                                <Redirect to="/statistics/dashboard" />
+                                            );
+                                        }}
+                                    />
                                     {/* Statistics */}
-                                    <Route path='/statistics/dashboard' component={DashboardPage} />
-                                    <Route path='/statistics/risk_ranking' component={RiskRankingPage} />
+                                    <Route
+                                        path="/statistics/dashboard"
+                                        component={DashboardPage}
+                                    />
+                                    <Route
+                                        path="/statistics/risk_ranking"
+                                        component={RiskRankingPage}
+                                    />
                                     {/* Not found */}
-                                    <Route path='*' component={NotFoundPage} />
+                                    <Route path="*" component={NotFoundPage} />
                                 </Switch>
                             </Layout>
                         </Route>
                     </Switch>
                 </Router>
             </MuiThemeProvider>
-        )
+        );
     }
 }
 
