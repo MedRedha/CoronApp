7c339207880d4e56c47b4facd9281160dfa8d129
Med Redha <redha.dirac@gmail.com>
2020-03-20 16:48:12 +0100

    recycling is good for the planet :wastebasket:

--- stickyboard.config.js
+++ stickyboard.config.js
@@ -2,6 +2,5 @@ module.exports = {
     title: 'CoronApp',
     description:
         'Global dashboard for monitoring Corona virus (COVID-19) tweaked by The WuuD® Team',
-    meta_image: '/static/image/CoronaBoard_preview.png',
     favicon: '/static/image/favicon.png',
 };
