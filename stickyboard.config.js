51a87c681f017d63c3aadb45acd376117e52b4b9
Med Redha <redha.dirac@gmail.com>
2020-03-20 19:54:22 +0100

    adding favicon image :heavy_plus_sign:

--- stickyboard.config.js
+++ stickyboard.config.js
@@ -2,5 +2,6 @@ module.exports = {
     title: 'CoronApp',
     description:
         'Global dashboard for monitoring Corona virus (COVID-19) tweaked by The WuuD® Team',
+    meta_image: '/static/image/favicon.png',
     favicon: '/static/image/favicon.png',
 };
