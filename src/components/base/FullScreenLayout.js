d38a2a69b93fbc67db42df12af3977a01b81a20d
Med Redha <redha.dirac@gmail.com>
2020-03-21 02:19:09 +0100

    adjusting full screen pages styling :lipstick:

--- src/components/base/FullScreenLayout.js
+++ src/components/base/FullScreenLayout.js
@@ -4,17 +4,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { withStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import { blueGrey } from '@material-ui/core/colors';
 
-const styles = theme => ({
+const styles = (theme) => ({
     root: {
         width: '100%',
         height: '100%',
-        display: 'flex',
-        justifyContent: 'center',
-        overflow: 'auto',
-        backgroundColor: theme.colors.contentBackground,
     },
 });
 
@@ -22,11 +16,7 @@ class FullScreenLayout extends React.Component {
     render() {
         const { classes, theme } = this.props;
 
-        return (
-            <div className={classes.root}>
-                {this.props.children}
-            </div>
-        )
+        return <div className={classes.root}>{this.props.children}</div>;
     }
 }
 
