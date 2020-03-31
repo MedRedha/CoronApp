24785438597e8fe480cf27e3bb582629e6910ac7
Med Redha <redha.dirac@gmail.com>
2020-03-31 17:05:56 +0100

    still working on the splashscreen :building_construction: :rotating_light:

--- src/components/page/LoadingPage.js
+++ src/components/page/LoadingPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Delayed from 'react-delayed';
+import ParticlesBg from 'particles-bg';
 import Welcome from 'react-welcome-page';
-import UIfx from 'uifx';
 import './../../static/css/loading.css';
 
 let text = <div className={'LogoText'}>WuuD Team</div>;
@@ -10,21 +10,69 @@ let logo = './../../static/image/WuuD.ico';
 
 export default class LoadingPage extends React.Component {
     render() {
+        let config = {
+            num: [5, 50],
+            rps: 0.25,
+            radius: [30],
+            rotate: [0, 20],
+            borderRadius: [50],
+            life: [0.5, 5],
+            v: [0.5, 1],
+            tha: [-40, 40],
+            alpha: [1, 0],
+            scale: [0.1, 0.5],
+            position: 'right',
+            color: [
+                'random',
+                '#ff505f',
+                '#FF0099',
+                '#0082c8',
+                '#29ffc6',
+                '#fc4a1a',
+                '#7303c0',
+                '#EF3B36',
+                '#ffffff',
+                '#fff900',
+            ],
+            cross: 'bround',
+            // emitter: 'follow',
+            f: [2, -1],
+            g: 1,
+        };
+
+        if (Math.random() > 0.85) {
+            config = Object.assign(config, {
+                onParticleUpdate: (ctx, particle) => {
+                    ctx.beginPath();
+                    ctx.rect(
+                        particle.p.x,
+                        particle.p.y,
+                        particle.radius * 2,
+                        particle.radius * 2
+                    );
+                    ctx.fillStyle = particle.color;
+                    ctx.fill();
+                    ctx.closePath();
+                },
+            });
+        }
         return (
             <div className="bg">
-                <Welcome
-                    loopDuration={2200}
-                    data={[
-                        {
-                            image: logo,
-                            imageAnimation: 'rotateIn',
-                            textAnimation: 'bounceIn',
-                            text: text,
-                            textColor: 'whitesmoke',
-                        },
-                    ]}
-                />
-                <Delayed mounted={true} mountAfter={2200} unmountAfter={4400}>
+                <Delayed mounted={true} mountAfter={1000}>
+                    <Welcome
+                        loopDuration={2200}
+                        data={[
+                            {
+                                image: logo,
+                                imageAnimation: 'rotateIn',
+                                textAnimation: 'bounceIn',
+                                text: text,
+                                textColor: 'whitesmoke',
+                            },
+                        ]}
+                    />
+                </Delayed>
+                <Delayed mounted={true} mountAfter={3200} unmountAfter={5400}>
                     <Welcome
                         loopDuration={2200}
                         data={[
@@ -38,18 +86,27 @@ export default class LoadingPage extends React.Component {
                         ]}
                     />
                 </Delayed>
-                <Delayed mounted={true} mountAfter={4400} unmountAfter={8800}>
+                <Delayed mounted={true} mountAfter={5400} unmountAfter={9800}>
                     <div>
                         <div className="body">
                             <div className="main">
-                                <img src={logo} alt={logo} />
-                                <div className="slogan anim-typewriter">
-                                    Developers by passion
+                                <img className="image" src={logo} alt={logo} />
+                                <div
+                                    id="typewriter"
+                                    className="slogan anim-typewriter">
+                                    Developers by Passion
                                 </div>
                             </div>
                         </div>
                     </div>
                 </Delayed>
+                <ParticlesBg
+                    num={120}
+                    type="custom"
+                    config={config}
+                    bg={false}
+                    color="#131313"
+                />
             </div>
         );
     }
